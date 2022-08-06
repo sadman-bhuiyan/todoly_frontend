a02b147f4ef3a077569e4b5116ce205bd9bec758
Sadman <bhuiyansad@gmail.com>
2022-08-06 12:45:25 +0200

    release

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -30,7 +30,15 @@ router.get('/todos', loggedIn, (req, res, next) => {
         }
         console.log("Connected to DB successfully!")
       });
-    res.status(200).json({message: "Success"})
+      connection.query('SELECT *  FROM Todos WHERE userID= ?',[req.user.id] , async (error, results) => {
+        console.log(results);
+        if(error){
+            res.status(503).json({message: "Error in getting todo"})
+            throw error;
+        }else{
+            res.status(200).json({todos: results})
+        }
+      });
 });
 
 router.post('/createtodos', loggedIn, (req, res,next)=>{
@@ -70,5 +78,25 @@ router.post('/updatetodos', loggedIn, (req, res,next)=>{
         }
       });
 });
+
+router.delete('/deletetodos', loggedIn, (req,res, next)=> {
+  connection.connect(err => {
+    if (err) {
+      console.log('Error connecting to db -> ' + err)
+      return;
+    }
+    console.log("Connected to DB successfully!")
+  });
+
+  connection.query('DELETE FROM Todos  WHERE id = ?', [req.body.todo_id], async (error) => {
+    if(error){
+        res.status(503).json({message: "Error in deleting todo"})
+        throw error;
+    }else{
+        res.status(200).json({message: "Todo deleted successfully!"})
+    }
+  });
+
+})
 
 module.exports = router;
